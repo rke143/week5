4b4a25da47d8afafb7c594342ff1b61e820357ef
Julia Vrn <julia.voronetskaja@tktk.ee>
2023-10-05 13:40:02 +0200

    week 5 HTTP

---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    const requestUrl = url.parse(request.url).pathname;
+    console.log(requestUrl);
+
+    if(requestUrl === '/') {
+        fs.readFile('index.html', (error, fileContent) => {
+            if(error){
+                response.writeHead(404);
+                response.write(`Error. File not found.`);
+            } else {
+                response.writeHead(200);
+                response.write(fileContent);
+            }
+
+            response.end();
+        });
+        
+    } else if (requestUrl === '/about') {
+        fs.readFile('about.html', (error, fileContent) => {
+            if(error){
+                response.writeHead(404);
+                response.write(`Error. File not found.`);
+            } else {
+                response.writeHead(200);
+                response.write(fileContent);
+            }
+
+            response.end();
+        });
+        
+    } else if (requestUrl === '/contact') {
+        response.write(`Hello from port ${requestUrl}.`);
+    } else {
+        response.writeHead(404);
+        response.write(`Error. ${requestUrl} page not found.`);
+        response.end();
+    }
+    
+    
+});
+
+//Ctrl + c
+
+server.listen(port, error => {
+    if(error){
+        console.log(error);
+    } else {
+        console.log(`Server is listening on port ${port}.`);
+    }
+});
